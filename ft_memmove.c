03678e7df58af5c9e241d09abb20366d219ba58d
umtalj0ta <joaogomesvieira95@gmail.com>
2023-04-15 20:31:58 +0100

    fix memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -11,7 +11,7 @@ void *ft_memmove(void *dst, const void *src, size_t len)
     psrc = (char*)src;
     i = 0;
     temp = malloc(sizeof(char) * len);
-    while (i < len)
+    while (i < (int)len)
     {
         temp[i] = psrc[i];
         i++;    
