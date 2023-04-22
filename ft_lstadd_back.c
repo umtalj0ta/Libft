50ac1ba8461cd273e38264546cb6a19fba5c58fc
umtalj0ta <joaogomesvieira95@gmail.com>
2023-04-22 18:02:07 +0100

    ft_lstadd_back

---
+++ ft_lstadd_back.c
@@ -0,0 +1,18 @@
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*curr;
+
+	if (!lst || !new)
+		return ;
+	if (!*lst)
+		*lst = new;
+	else
+	{
+		curr = *lst;
+		while (curr->next)
+			curr = curr->next;
+		curr->next = new;
+	}
+}
